57517648a1a6349fb046d3eb8a3081d8b762f8c7
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-06-22 19:50:33 -0500

    added the firebase config key

---
+++ my-app/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDzhwKC_WRm23KL9gY4wmU1otKuk6vc8C8",
+    authDomain: "mentalhealthmessaging-app.firebaseapp.com",
+    projectId: "mentalhealthmessaging-app",
+    storageBucket: "mentalhealthmessaging-app.appspot.com",
+    messagingSenderId: "853829078114",
+    appId: "1:853829078114:web:b3f203ba7db0b0ca03d53b"
+  };
