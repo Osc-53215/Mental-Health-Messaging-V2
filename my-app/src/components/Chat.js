5773a6a70bef9bad529f34d1030a6b44664bf3a1
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-06-23 22:06:12 -0500

    created onlick and not going to add logic to send message const im crreating

--- my-app/src/components/Chat.js
+++ my-app/src/components/Chat.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
 
 function Chat({channelName, channelId}) {
+
+    const sendMessage = e => {
+        e.preventDefault();
+    }
+
     return (
         <div className = 'chat__input'>
             <form>
-                <input
-                placeholder = {`Message #ROOM`}
-                />
+                <input placeholder = {`Message #ROOM`}/>
+
+                <Button hidden type = 'submit' onClick = {sendMessage}>
+                    SEND
+                </Button>
+
             </form>
         </div>
     )
