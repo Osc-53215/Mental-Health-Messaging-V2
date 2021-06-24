a446695fcfa90ea85715008525bfb427b2478a8a
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-06-23 22:24:20 -0500

    creating divs to help me organize when styling

--- my-app/src/components/Chat.js
+++ my-app/src/components/Chat.js
@@ -4,7 +4,19 @@ import './Chat.css'
 function Chat() {
     return (
         <div className = 'chat'>
+            
             <h1>I am the chat screen</h1>
+
+            <div className = 'chat__header'>
+                <div className = 'chat__headerleft'>
+
+                </div>
+
+                <div className = 'chat__headerright'>
+
+                </div>
+            </div>
+
         </div>
     )
 }
