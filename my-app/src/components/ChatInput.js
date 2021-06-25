92943d1aef85ee29610faf4d2fc03b61d85b58c8
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-06-24 19:36:44 -0500

    setting up schema for backend on firebase

--- my-app/src/components/ChatInput.js
+++ my-app/src/components/ChatInput.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Button } from '@material-ui/core';
-import './ChatInput.css'
+import './ChatInput.css';
+import firebase from 'firebase';
+import { db } from '../firebase';
 
 function Chat({channelName, channelId}) {
+    const [input, setInput] = useState('')
 
     const sendMessage = e => {
         e.preventDefault();
+
+        if (channelId) {
+            return false;
+        }
+
+        db.collections('rooms').doc(channelId).collection('messages').add({
+            message: input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        });
+
     }
 
     return (
         <div className = 'chat__input'>
             <form>
-                <input placeholder = {`Message #ROOM`}/>
+                <input 
+                value = {input}
+                onChange = {(e) => setInput(e.target.value)}
+                placeholder = {`Message #ROOM`}/>
 
                 <Button hidden type = 'submit' onClick = {sendMessage}>
                     SEND
