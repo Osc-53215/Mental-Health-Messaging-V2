2d945c0db096d76e919d7f0a0204d11c994a032d
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-06-25 23:55:21 -0500

    added sign out sunctionality when you press your avatar you automataiclly get signed out from the app

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -4,13 +4,20 @@ import {Avatar} from '@material-ui/core';
 import  AccessTimeIcon  from '@material-ui/icons/AccessTime';
 import SearchIcon from '@material-ui/icons/Search'
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../firebase'
 
 function Header() {
+    const [user] = useAuthState(auth);
+
     return (
         <div className = 'header'>
             <div className = 'header__left'>
                 <div className = 'header__avatar'>
-                    <Avatar />
+                    <Avatar 
+                    onClick = {() => auth.signOut()}
+                    atl = {user?.displayName} 
+                    src = {user?.photoUrl}/>
                 </div>
 
                     <AccessTimeIcon />
