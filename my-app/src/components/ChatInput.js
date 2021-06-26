7f83bc4d5aa2c759e36f86d4b6475a686d6736fc
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-06-26 00:15:28 -0500

    got users image to pop up whenever you chat in a text box!

--- my-app/src/components/ChatInput.js
+++ my-app/src/components/ChatInput.js
@@ -2,10 +2,12 @@ import React, { useRef, useState } from 'react';
 import { Button } from '@material-ui/core';
 import './ChatInput.css';
 import firebase from 'firebase';
-import { db } from '../firebase';
+import { auth, db } from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 function Chat({channelName, channelId, chatRef}) {
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState('');
+    const [user] = useAuthState(auth);
 
     const sendMessage = e => {
         e.preventDefault();
@@ -17,8 +19,8 @@ function Chat({channelName, channelId, chatRef}) {
         db.collection('rooms').doc(channelId).collection('messages').add({
             message: input,
             timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-            user: 'Elizandra Sandoval',
-            userImage: 'https://scontent-ort2-2.xx.fbcdn.net/v/t1.6435-9/170122139_4045199688874900_4382704728184763151_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=1IMzvLGdYLMAX_P97qi&_nc_ht=scontent-ort2-2.xx&oh=ba77bd6732768b076fda497ddf1123ff&oe=60E68777'
+            user: user.displayName,
+            userImage: user.photoURL
         });
 
         chatRef?.current?.scrollIntoView({behavior: 'smooth'});
