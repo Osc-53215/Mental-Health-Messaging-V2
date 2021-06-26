47a1b502ee2263f5e24d5b49d640c5fe8d4a8de4
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-06-25 23:45:43 -0500

    implemented the google sign in with one line of code

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Login.css';
 import logo from './photos/mh-image.PNG';
 import { Button } from '@material-ui/core'
+import { auth, provider } from '../firebase';
 
 function Login() {
     
     const singIn = (e) => {
         e.preventDefault();
+
+        auth.signInWithPopup(provider).catch((error) => alert(error.message));
         
     }
 
@@ -19,7 +22,7 @@ function Login() {
                 <h1>Sign in to Uni-Health</h1>
                 <p>UniHealth.com</p>
 
-                <Button type = 'submit' onClick = {singIn}>
+                <Button onClick = {singIn}>
                     Sign in with Google
                 </Button>
 
