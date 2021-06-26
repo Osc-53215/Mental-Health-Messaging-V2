ad5f153140069e5956174fc1fca72f01ef1ad70b
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-06-25 23:35:02 -0500

    adding the google sing in portion to the login menu

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -5,10 +5,20 @@ import logo from './photos/mh-image.PNG'
 function Login() {
     return (
         <div className = 'login'>
-            
+
             <div className = 'login__inner'>
                 <img src = {logo} alt = ''/>
+
+                <h1>Sign in to the Uni-Health</h1>
+                <p>UniHealth.com</p>
+
+                <Button type = 'submit' onClick = {singin}>
+                    Sing in with Google
+                </Button>
+
             </div>
+
+            
             
         </div>
     )
